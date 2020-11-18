300674e9c3f550bd36cdd17841d0d21715b552a1
Imam Syuhada <imam.sy11@gmail.com>
2020-11-19 00:42:32 +0700

    insert media succed

--- api-gateway/app.js
+++ api-gateway/app.js
@@ -4,7 +4,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const courseRouter = require('./routes/courses');
 const mediaRouter = require('./routes/media');
@@ -14,12 +13,11 @@ const paymentRouter = require('./routes/payments');
 const app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit:'50mb'}));
+app.use(express.urlencoded({ extended: false, limit:'50mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/courses', courseRouter);
 app.use('/media', mediaRouter);
