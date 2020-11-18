713a6b147687ac1ed38fd6bb273edac2b0d83c16
Imam Syuhada <imam.sy11@gmail.com>
2020-11-19 00:47:38 +0700

    get all media

--- api-gateway/routes/media.js
+++ api-gateway/routes/media.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const mediaHandler = require('./handler/media')
 
-/* GET users listing. */
 router.post('/', mediaHandler.create);
+router.get('/', mediaHandler.getAll)
 
 module.exports = router;
