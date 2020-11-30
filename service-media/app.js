889bb563ff143c2e5b6c106ddc8bf5265d7e88f6
Imam Syuhada <imam.sy11@gmail.com>
2020-12-01 01:10:50 +0700

    service media erase root index html

--- service-media/app.js
+++ service-media/app.js
@@ -12,7 +12,6 @@ app.use(logger('dev'));
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ extended: false, limit: '50mb' }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/media', mediaRouter);
 
