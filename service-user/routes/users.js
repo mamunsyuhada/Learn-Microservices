a0ec1aff90bdf517a27265511990e080121274ab
Imam Syuhada <imam.sy11@gmail.com>
2020-12-01 01:59:17 +0700

    update profil user

--- service-user/routes/users.js
+++ service-user/routes/users.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const usersHandler = require('./handler/users');
 
 router.post('/register/:role', usersHandler.register)
+router.post('/login', usersHandler.login)
+router.put('/update/:id', usersHandler.update)
 
 module.exports = router;
